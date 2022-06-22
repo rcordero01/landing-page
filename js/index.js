b79a485b1a0318041a40aace2c7b3b93c657167b
Ruben Cordero <rubencordero01@gmail.com>
2022-06-21 21:19:41 -0500

    Makes mods to timer function

--- js/index.js
+++ js/index.js
@@ -1,26 +1,32 @@
-
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+  var timer = duration,
+    minutes,
+    seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+    display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
 }
 
+document.querySelector("btn"), addEventListener("click", handleClick);
 
+function handleClick() {
+  var twentyFiveMinutes = 60 * 25,
+    display = document.querySelector("#time");
+  startTimer(twentyFiveMinutes, display);
+}
 
-window.onload = function () {
-    var twentyFiveMinutes = 60 * 25,
-        display = document.querySelector('#time');
-    startTimer(twentyFiveMinutes, display);
-};
-
+// window.onload = function () {
+//   var twentyFiveMinutes = 60 * 25,
+//     display = document.querySelector("#time");
+//   startTimer(twentyFiveMinutes, display);
+// };
